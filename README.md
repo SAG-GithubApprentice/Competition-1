5ecba2336d85a30082c779334dfce544d15b5a19
SAG-GithubApprentice <garzon1981@hotmail.com>
2023-12-29 00:43:42 -0700

    Competition 1

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Competition-1
+
+Summary:
+The code processes variations of "St. Albans," and standardizes them to a consistent format. It capitalizes each word, removes periods and commas and handles a specific case ("St.Al bans") by replacing it with "St Albans." The result is a list of cleaned and standardized names.
+
+The code takes a list of names with various formats, cleans them by standardizing capitalization and handling specific cases, and produces a new list of cleaned names. The primary tools used are string manipulation, list comprehensions, and uses: .join(), .replace() and .split() methods.
+
+For each name in the original list (names):
+    Replace the specific case "St.Al bans" with "St Albans."
+    Replace periods and commas with spaces.
+    Split the modified string into a list of words.
+    Capitalize each word in the list.
+    Join the capitalized words back into a single string.
+    The result is a list of cleaned and standardized names stored in "new_names."
---
+++ list_challenge.py
@@ -0,0 +1,25 @@
+names = ['St. Albans',
+        'St. Albans', 
+        'St Albans', 
+        'St.Ablans',
+        "St.albans", 
+        "St. Alans", 'S.Albans',
+        'St..Albans', 'S.Albnas', 
+        'St. Albnas', "St.Al bans", 'St.Algans',
+        "Sl.Albans", 'St. Allbans', "St, Albans", 'St. Alban', 'St. Alban']
+
+#Expected output:
+
+#['St Albans', 'St Albans', 'St Albans', 'St Ablans', 
+# 'St Albans', 'St Alans', 'S Albans', 'St Albans', 'S Albnas', 
+# 'St Albnas', 'St Albans', 'St Algans', 'Sl Albans', 'St Allbans', 'St Albans', 'St Alban', 'St Alban']
+
+new_names = [' '.join(word.capitalize() for word in name.replace("St.Al bans", 'St Albans')
+                       .replace('.', ' ').replace(',', ' ').split())for name in names]
+
+print("new names =", new_names)
+
+#Actual output
+#new names = ['St Albans', 'St Albans', 'St Albans', 'St Ablans', 
+# 'St Albans', 'St Alans', 'S Albans', 'St Albans', 'S Albnas',
+# 'St Albnas', 'St Albans', 'St Algans', 'Sl Albans', 'St Allbans', 'St Albans', 'St Alban', 'St Alban']
