c9eb150b52f53bd2d359f12096c42c503213daee
SAG-GithubApprentice <garzon1981@hotmail.com>
2023-12-30 16:36:33 -0700

    Submission 30Dec2023

--- list_challenge.py
+++ list_challenge.py
@@ -20,6 +20,7 @@ new_names = [' '.join(word.capitalize() for word in name.replace("St.Al bans", '
 print("new names =", new_names)
 
 #Actual output
+
 #new names = ['St Albans', 'St Albans', 'St Albans', 'St Ablans', 
 # 'St Albans', 'St Alans', 'S Albans', 'St Albans', 'S Albnas',
 # 'St Albnas', 'St Albans', 'St Algans', 'Sl Albans', 'St Allbans', 'St Albans', 'St Alban', 'St Alban']
